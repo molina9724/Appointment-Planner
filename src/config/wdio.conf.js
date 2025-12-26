97c9e0fdeef52fffc94cac5f7212072a6e47ddbb
Daniel Molina <daniel.molina_epam.com>
2025-12-26 17:19:52 -0500

    baseUrl fix

--- src/config/wdio.conf.js
+++ src/config/wdio.conf.js
@@ -85,7 +85,7 @@ exports.config = {
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
   baseUrl:
-    "https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/patients",
+    "https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard",
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
